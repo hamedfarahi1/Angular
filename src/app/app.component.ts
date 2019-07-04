a81d5d1be79d1d76cff93d7635b462ec71965669
Hamed <hamedfarahif@gmail.com>
2019-07-04 22:20:38 +0430

    first commit with sample request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Component } from "@angular/core";
 
+const httpOptions = {
+  headers: new HttpHeaders({ "Content-Type": "application/json" })
+};
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'AppTest';
+  Message: any;
+  ret: string;
+  constructor(private http: HttpClient) {
+    this.Message = "Menu";
+  }
+
+  /** GET JSONes from the server */
+  getJSONes() {
+    console.log(
+      this.http
+        .get<string>("https://jsonplaceholder.typicode.com/todos/1")
+        .subscribe(res => {
+          console.log(res["title"]);
+          this.Message = res["title"];
+        })
+    );
+  }
 }
